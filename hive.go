7b3278c21d8912537f58175e14163eb369426127
Eric Fode <ericfode@gmail.com>
2012-08-18 12:00:34 -0400

    adding some missing files

--- hive.go
+++ hive.go
@@ -14,6 +14,19 @@ type User struct {
 	Github     string
 }
 
+type StreamItem struct {
+	Pic      string
+	UserName string
+	JIT      string
+}
+
+func dummyStreamItem() *StreamItem {
+	item := &StreamItem{
+		Pic:      "http://0.gravatar.com/avatar/b7ddec29f78231d1a59752134b1f57fb",
+		UserName: "ericfode",
+		JIT:      "This might actaully end up being kinda cool"}
+	return item
+}
 func dummyUser() *User {
 	user := &User{
 		Pic:        "http://0.gravatar.com/avatar/b7ddec29f78231d1a59752134b1f57fb",
@@ -31,6 +44,30 @@ func renderProfile() string {
 	return html
 }
 
+func renderSideBar() string {
+
+}
+
+func renderFollowers() string {
+
+}
+
+func renderFollowing() string {
+
+}
+
+func renderStream() string {
+	items := []*streamItem{
+		dummyStreamItem(), dummyStreamItem(), dummyStreamItem()}
+	html := mustache.RenderFileInLayout("Pages/Stream.mustache", "Pages/layout.mustache", items)
+	return html
+}
+
+func renderStreamItem() string {
+	item := dummyStreamItem()
+	html := mustache.RenderInLayout("Pages/StreamItem.mustache", "Pages/layout.mustache", item)
+}
+
 func renderCSS(val string) string {
 
 	if css, err := ioutil.ReadFile("Pages/CSS/" + val); err != nil {
@@ -42,8 +79,25 @@ func renderCSS(val string) string {
 
 }
 
+func renderIMG(val string) string{
+	
+}
+
+func renderJS(val string) string {
+
+	if css, err := ioutil.ReadFile("Pages/JS/" + val); err != nil {
+		return err.Error()
+	} else {
+		return string(css)
+	}
+	return ""
+
+}
+
 func main() {
 	web.Get("/", renderProfile)
 	web.Get("/CSS/(.*)", renderCSS)
+	web.Get("/JS/(.*)", renderJS)
+	web.Get("/Stream", renderStream)
 	web.Run("0.0.0.0:9998")
 }
