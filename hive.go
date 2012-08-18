d1ad1ffc61f9faa6825a32d4430e76d56ff44368
Eric Fode <ericfode@gmail.com>
2012-08-18 11:31:55 -0400

    Finished up profile card

--- hive.go
+++ hive.go
@@ -2,6 +2,7 @@ package main
 
 import "github.com/hoisie/web"
 import "github.com/hoisie/mustache"
+import "io/ioutil"
 
 type User struct {
 	Pic        string
@@ -20,17 +21,29 @@ func dummyUser() *User {
 		UserName:   "ericfode",
 		Email:      "ericfode@gmail.com",
 		Bio:        "My Awsome Bio",
-		Skills:     "Everthing",
+		Skills:     "Everthing, Being Awesome, Spelling badly, Stack Smashing, Makeing explosives",
 		Github:     "github.com/ericfode"}
 	return user
 }
 
-func renderProfile(val string) string {
-	html := mustache.RenderFileInLayout("DisplayProfile.mustache", "layout.mustache", dummyUser())
+func renderProfile() string {
+	html := mustache.RenderFileInLayout("Pages/DisplayProfile.mustache", "Pages/layout.mustache", dummyUser())
 	return html
 }
 
+func renderCSS(val string) string {
+
+	if css, err := ioutil.ReadFile("Pages/CSS/" + val); err != nil {
+		return err.Error()
+	} else {
+		return string(css)
+	}
+	return ""
+
+}
+
 func main() {
-	web.Get("/(.*)", renderProfile)
+	web.Get("/", renderProfile)
+	web.Get("/CSS/(.*)", renderCSS)
 	web.Run("0.0.0.0:9998")
 }
